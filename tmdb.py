889122bb15652edae99f42571c9b7aba027ecebb
Doğan Aydın <dogan1aydin@gmail.com>
2012-11-08 19:29:38 +0200

    Movies-Movie

--- tmdb.py
+++ tmdb.py
@@ -21,25 +21,25 @@ config = {}
 def configure(api_key):
     config['apikey'] = api_key
     config['urls'] = {}
-    config['urls']['movie.search'] = "http://api.themoviedb.org/3/search/movie?query=%%s&api_key=%(apikey)s&page=%%s" % (config)
-    config['urls']['movie.info'] = "http://api.themoviedb.org/3/movie/%%s?api_key=%(apikey)s" % (config)
-    config['urls']['people.search'] = "http://api.themoviedb.org/3/search/person?query=%%s&api_key=%(apikey)s&page=%%s" % (config)
-    config['urls']['collection.info'] = "http://api.themoviedb.org/3/collection/%%s&api_key=%(apikey)s" % (config)
-    config['urls']['movie.alternativetitles'] = "http://api.themoviedb.org/3/movie/%%s/alternative_titles?api_key=%(apikey)s" % (config)
-    config['urls']['movie.casts'] = "http://api.themoviedb.org/3/movie/%%s/casts?api_key=%(apikey)s" % (config)
-    config['urls']['movie.images'] = "http://api.themoviedb.org/3/movie/%%s/images?api_key=%(apikey)s" % (config)
-    config['urls']['movie.keywords'] = "http://api.themoviedb.org/3/movie/%%s/keywords?api_key=%(apikey)s" % (config)
-    config['urls']['movie.releases'] = "http://api.themoviedb.org/3/movie/%%s/releases?api_key=%(apikey)s" % (config)
-    config['urls']['movie.trailers'] = "http://api.themoviedb.org/3/movie/%%s/trailers?api_key=%(apikey)s" % (config)
-    config['urls']['movie.translations'] = "http://api.themoviedb.org/3/movie/%%s/translations?api_key=%(apikey)s" % (config)
-    config['urls']['person.info'] = "http://api.themoviedb.org/3/person/%%s&api_key=%(apikey)s" % (config)
-    config['urls']['person.credits'] = "http://api.themoviedb.org/3/person/%%s/credits?api_key=%(apikey)s" % (config)
-    config['urls']['person.images'] = "http://api.themoviedb.org/3/person/%%s/images?api_key=%(apikey)s" % (config)
-    config['urls']['latestmovie'] = "http://api.themoviedb.org/3/latest/movie?api_key=%(apikey)s" % (config)
-    config['urls']['config'] = "http://api.themoviedb.org/3/configuration?api_key=%(apikey)s" % (config)
-    config['urls']['request.token'] = "http://api.themoviedb.org/3/authentication/token/new?api_key=%(apikey)s" % (config)
-    config['urls']['session.id'] = "http://api.themoviedb.org/3/authentication/session/new?api_key=%(apikey)s&request_token=%%s" % (config)
-    config['urls']['movie.add.rating'] = "http://api.themoviedb.org/3/movie/%%s/rating?session_id=%%s&api_key=%(apikey)s" % (config)
+    config['urls']['movie.search'] = "https://api.themoviedb.org/3/search/movie?query=%%s&api_key=%(apikey)s&page=%%s" % (config)
+    config['urls']['movie.info'] = "https://api.themoviedb.org/3/movie/%%s?api_key=%(apikey)s" % (config)
+    config['urls']['people.search'] = "https://api.themoviedb.org/3/search/person?query=%%s&api_key=%(apikey)s&page=%%s" % (config)
+    config['urls']['collection.info'] = "https://api.themoviedb.org/3/collection/%%s&api_key=%(apikey)s" % (config)
+    config['urls']['movie.alternativetitles'] = "https://api.themoviedb.org/3/movie/%%s/alternative_titles?api_key=%(apikey)s" % (config)
+    config['urls']['movie.casts'] = "https://api.themoviedb.org/3/movie/%%s/casts?api_key=%(apikey)s" % (config)
+    config['urls']['movie.images'] = "https://api.themoviedb.org/3/movie/%%s/images?api_key=%(apikey)s" % (config)
+    config['urls']['movie.keywords'] = "https://api.themoviedb.org/3/movie/%%s/keywords?api_key=%(apikey)s" % (config)
+    config['urls']['movie.releases'] = "https://api.themoviedb.org/3/movie/%%s/releases?api_key=%(apikey)s" % (config)
+    config['urls']['movie.trailers'] = "https://api.themoviedb.org/3/movie/%%s/trailers?api_key=%(apikey)s" % (config)
+    config['urls']['movie.translations'] = "https://api.themoviedb.org/3/movie/%%s/translations?api_key=%(apikey)s" % (config)
+    config['urls']['person.info'] = "https://api.themoviedb.org/3/person/%%s&api_key=%(apikey)s" % (config)
+    config['urls']['person.credits'] = "https://api.themoviedb.org/3/person/%%s/credits?api_key=%(apikey)s" % (config)
+    config['urls']['person.images'] = "https://api.themoviedb.org/3/person/%%s/images?api_key=%(apikey)s" % (config)
+    config['urls']['latestmovie'] = "https://api.themoviedb.org/3/latest/movie?api_key=%(apikey)s" % (config)
+    config['urls']['config'] = "https://api.themoviedb.org/3/configuration?api_key=%(apikey)s" % (config)
+    config['urls']['request.token'] = "https://api.themoviedb.org/3/authentication/token/new?api_key=%(apikey)s" % (config)
+    config['urls']['session.id'] = "https://api.themoviedb.org/3/authentication/session/new?api_key=%(apikey)s&request_token=%%s" % (config)
+    config['urls']['movie.add.rating'] = "https://api.themoviedb.org/3/movie/%%s/rating?session_id=%%s&api_key=%(apikey)s" % (config)
     config['api'] = {}
     config['api']['backdrop.sizes'] = ""
     config['api']['base.url'] = ""
@@ -91,8 +91,8 @@ class Core(object):
         config['api']['session.id'] = sess["session_id"]
         return sess["session_id"]
 
-class Movie(Core):
-    def __init__(self, title="", id=-1, limit=False):
+class Movies(Core):
+    def __init__(self, title="", limit=False):
         self.limit = limit
         self.update_configuration()
         title = self.escape(title)
@@ -101,17 +101,12 @@ class Movie(Core):
         pages = self.movies["total_pages"]
         if not self.limit:
             for i in range(int(pages)):
-                self.movies["results"].extend(self.getJSON(config['urls']['movie.search'] % (title,str(i)))["results"]) 
-        if id > -1:
-            self.movies_full = self.getJSON(config['urls']['movie.info'] % id)
-
-    def full_info(self,movie_id):
-        self.movies_full = self.getJSON(config['urls']['movie.info'] % str(movie_id))
+                self.movies["results"].extend(self.getJSON(config['urls']['movie.search'] % (title,str(i)))["results"])
 
     def get_total_results(self):
         if self.limit:
             return len(self.movies["results"])
-        return self.movies["total_results"]   
+        return self.movies["total_results"]
 
     def get_id(self,movie_index=0):
         return self.movies["results"][movie_index]["id"]
@@ -138,118 +133,86 @@ class Movie(Core):
         img_path = self.movies["results"][movie_index]["poster_path"]
         return config['api']['base.url']+self.poster_sizes(img_size)+img_path
 
-    def is_adult(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['adult']
+class Movie(Core):
+    def __init__(self,movie_id):
+        self.movie_id = movie_id
+        self.update_configuration()
+        self.movies = self.getJSON(config['urls']['movie.info'] % self.movie_id)
 
-    def get_collection_id(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['belongs_to_collection']["id"]
+    def is_adult(self):
+        return self.movies['adult']
 
-    def get_collection_name(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['belongs_to_collection']["name"]
+    def get_collection_id(self):
+        return self.movies['belongs_to_collection']["id"]
+
+    def get_collection_name(self):
+        return self.movies['belongs_to_collection']["name"]
 
     # Sizes = s->w300 m->w780 l->w1280 o->original(default)
-    def get_collection_backdrop(self,img_size="o",movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        img_path = self.movies_full["belongs_to_collection"]["backdrop_path"]
+    def get_collection_backdrop(self,img_size="o"):
+        img_path = self.movies["belongs_to_collection"]["backdrop_path"]
         return config['api']['base.url']+self.poster_sizes(img_size)+img_path
 
-    # Sizes = s->w92 m->w185 l->w500 o->original(default)    
-    def get_collection_poster(self,img_size="o",movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        img_path = self.movies_full["belongs_to_collection"]["poster_path"]
+    # Sizes = s->w92 m->w185 l->w500 o->original(default)
+    def get_collection_poster(self,img_size="o"):
+        img_path = self.movies["belongs_to_collection"]["poster_path"]
         return config['api']['base.url']+self.poster_sizes(img_size)+img_path
 
-    def get_budget(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['budget']
+    def get_budget(self):
+        return self.movies['budget']
 
-    def get_genres(self,movie_id=0):
+    def get_genres(self):
         genres = []
-        if movie_id > 0:
-            self.full_info(movie_id)
-        for i in self.movies_full['genres']:
+        for i in self.movies['genres']:
             genres.append({"id":i["id"],"name":i["name"]})
         return genres
 
-    def get_homepage(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['homepage']
+    def get_homepage(self):
+        return self.movies['homepage']
 
-    def get_imdb_id(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['imdb_id']
+    def get_imdb_id(self):
+        return self.movies['imdb_id']
 
-    def get_overview(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
+    def get_overview(self):
         return self.movies_full['overview']
 
-    def get_production_companies(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        for i in self.movies_full['production_companies']:
+    def get_production_companies(self):
+        for i in self.movies['production_companies']:
             companies = {"id":i["id"],"name":i["name"]}
         return companies
 
-    def get_productions_countries(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        for i in self.movies_full['production_countries']:
+    def get_productions_countries(self):
+        for i in self.movies['production_countries']:
             countries = {"iso_3166":i["iso_3166"],"name":i["name"]}
         return countries
 
-    def get_revenue(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['revenue']
+    def get_revenue(self):
+        return self.movies['revenue']
 
-    def get_runtime(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['runtime']
+    def get_runtime(self):
+        return self.movies['runtime']
 
-    def get_spoken_languages(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        for i in self.movies_full['spoken_languages']:
+    def get_spoken_languages(self):
+        for i in self.movies['spoken_languages']:
             langs = {"iso_639_1":i["iso_639_1"],"name":i["name"]}
         return langs
 
-    def get_tagline(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['tagline']
+    def get_tagline(self):
+        return self.movies['tagline']
 
-    def get_vote_average(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['vote_average']
+    def get_vote_average(self):
+        return self.movies['vote_average']
 
-    def get_vote_count(self,movie_id=0):
-        if movie_id > 0:
-            self.full_info(movie_id)
-        return self.movies_full['vote_count']
+    def get_vote_count(self):
+        return self.movies['vote_count']
 
-    def add_rating(self,value,movie_id=0):
+    def add_rating(self,value):
         if isinstance(value,float) or isinstance(value,int):
-            if movie_id > 0:
-                self.full_info(movie_id)
             if config["api"]["session.id"] == "":
                 return "PROBLEM_AUTH"
             sess_id = config["api"]["session.id"]
             data = {"value":float(value)}
-            req = requests.post(config['urls']['movie.add.rating'] % (movie_id,sess_id),data=data)
+            req = requests.post(config['urls']['movie.add.rating'] % (self.movie_id,sess_id),data=data)
             res = simplejson.loads(req.content)
             if res['status_message'] == "Success":
                 return True
